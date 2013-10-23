51cee6cb1499132a615ad3529c8f7d1ba107b5d6
Clarky158 <benclark.btc@gmail.com>
2013-10-23 16:10:36 +0100

    Added modifier texture

--- electro/block/basic/glassModifier.java
+++ electro/block/basic/glassModifier.java
@@ -1,6 +1,9 @@
 package assets.electrolysm.electro.block.basic;
 
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
 import assets.electrolysm.electro.electrolysmCore;
+import net.minecraft.client.renderer.texture.IconRegister;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
@@ -30,5 +33,11 @@ public class glassModifier extends Item {
     	}
     	
     	return false;
+    }
+	
+	@SideOnly(Side.CLIENT)
+    public void registerIcons(IconRegister par1IconRegister)
+    {
+        this.itemIcon = par1IconRegister.registerIcon("electrolysm:" + "glassModifier");
     }
 }
