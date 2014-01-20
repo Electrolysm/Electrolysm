246da7fe2092bf96df24dad2c870f22d7b993020
Clarky158 <benclark.btc@gmail.com>
2014-01-20 22:33:40 +0000

    Created the earthing block textures

--- electro/powerSystem/advEarther.java
+++ electro/powerSystem/advEarther.java
@@ -1,13 +1,20 @@
 package assets.electrolysm.electro.powerSystem;
 
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
 import assets.electrolysm.electro.powerSystem.te.TileEntityAdvEarther;
 import net.minecraft.block.BlockContainer;
 import net.minecraft.block.material.Material;
+import net.minecraft.client.renderer.texture.IconRegister;
 import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.Icon;
 import net.minecraft.world.World;
 
 public class advEarther extends BlockContainer {
 
+	@SideOnly(Side.CLIENT)
+	private Icon blockTop;
+	
 	public advEarther(int id, Material mat) {
 		super(id, Material.iron);
 		// TODO Auto-generated constructor stub
@@ -17,6 +24,28 @@ public class advEarther extends BlockContainer {
 	public TileEntity createNewTileEntity(World world) {
 		// TODO Auto-generated method stub
 		return new TileEntityAdvEarther();
+	}
+	
+	@SideOnly(Side.CLIENT)
+	@Override
+	public void registerIcons(IconRegister reg)
+	{
+		this.blockIcon = reg.registerIcon("electrolysm:" + "advEartherSide");
+		this.blockTop = reg.registerIcon("electrolysm:" + "advEartherTop");
+	}
+	
+	@Override
+	@SideOnly(Side.CLIENT)
+	public Icon getIcon(int side, int meta)
+	{
+		if(side == 1 || side == 0)
+		{
+			return this.blockTop;
+		}
+		else
+		{
+			return this.blockIcon;
+		}
 	}
 
 }
--- electro/powerSystem/earther.java
+++ electro/powerSystem/earther.java
@@ -2,12 +2,19 @@ package assets.electrolysm.electro.powerSystem;
 
 import net.minecraft.block.BlockContainer;
 import net.minecraft.block.material.Material;
+import net.minecraft.client.renderer.texture.IconRegister;
 import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.Icon;
 import net.minecraft.world.World;
 import assets.electrolysm.electro.electrolysmCore;
 import assets.electrolysm.electro.powerSystem.te.TileEntityEarther;
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
 
 public class earther extends BlockContainer {
+
+	@SideOnly(Side.CLIENT)
+	private Icon blockTop;
 
 	public earther(int id, Material mat) {
 		super(id, Material.iron);
@@ -22,6 +29,28 @@ public class earther extends BlockContainer {
 	public TileEntity createNewTileEntity(World world) {
 		// TODO Auto-generated method stub
 		return new TileEntityEarther();
+	}
+	
+	@SideOnly(Side.CLIENT)
+	@Override
+	public void registerIcons(IconRegister reg)
+	{
+		this.blockIcon = reg.registerIcon("electrolysm:" + "eartherSide");
+		this.blockTop = reg.registerIcon("electrolysm:" + "eartherTop");
+	}
+	
+	@Override
+	@SideOnly(Side.CLIENT)
+	public Icon getIcon(int side, int meta)
+	{
+		if(side == 1 || side == 0)
+		{
+			return this.blockTop;
+		}
+		else
+		{
+			return this.blockIcon;
+		}
 	}
 	
 }
