766d2bb6e69fc30644654d9e76093574a1b869d5
Clarky158 <benclark.btc@gmail.com>
2014-02-20 11:54:55 +0000

    Create NewsCheck.java

---
+++ electro/handlers/NewsCheck.java
@@ -0,0 +1,52 @@
+package assets.electrolysm.electro.handlers;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+
+public class NewsCheck {
+
+	private static String line;
+	public static boolean checkDone;
+	public static String chatMessage;
+	public static String version;
+
+	public static void check(){
+
+			System.out.println("[Electrolysm]Starting News Check");
+
+			URL url = null;
+			String inputLine = "";
+
+			try {
+				url = new URL("https://raw.github.com/Clarky158/Electrolysm/master/news.xml");
+			} catch (MalformedURLException e) {
+				e.printStackTrace();
+			}
+			BufferedReader in;
+			try {
+				URLConnection con = url.openConnection();
+				con.setReadTimeout( 1000 ); //1 second
+				in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+				while ((inputLine = in.readLine()) != null) {
+					System.out.println("[Electrolysm]" + inputLine);
+					line = "[Electrolysm]" + inputLine;
+				}
+		 	   in.close();
+
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			if(line != null)
+			{
+        chatMessage = line;
+			}
+
+			//System.out.println("[Electrolysm]" + version);
+			System.out.println("[Electrolysm]Ended News Check");
+			checkDone = true;
+	}
+}
