456248c67abf501dbb66b41a3b2a03a3ade576d3
Clarky158 <benclark.btc@gmail.com>
2014-03-31 22:37:48 +0100

    Create TileEntityElectrical.java

---
+++ electro/oreProccessing/te/TileEntityElectrical.java
@@ -0,0 +1,86 @@
+package assets.electrolysm.electro.oreProccessing.te;
+
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.tileentity.TileEntity;
+import net.minecraftforge.common.ForgeDirection;
+import universalelectricity.api.UniversalClass;
+import universalelectricity.api.energy.EnergyStorageHandler;
+import universalelectricity.api.energy.IEnergyInterface;
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
+
+@UniversalClass
+public class TileEntityElectrical extends TileEntity implements IEnergyInterface{
+
+	@SideOnly(Side.CLIENT)
+	public EnergyStorageHandler energy;
+	public long currentEnergy;
+
+    public TileEntityElectrical(long capacity)
+    {
+    	if(capacity > 0)
+    	{
+    		energy = new EnergyStorageHandler(capacity, Long.MAX_VALUE, 0);
+    	}
+    	else
+    	{
+    		energy = new EnergyStorageHandler(500000, Long.MAX_VALUE, 0);
+    	}
+    }
+    
+    @Override
+    public void readFromNBT(NBTTagCompound nbt)
+    {
+        super.readFromNBT(nbt);
+        this.energy.readFromNBT(nbt);
+    }
+
+    @Override
+    public void writeToNBT(NBTTagCompound nbt)
+    {
+        super.writeToNBT(nbt);
+        this.energy.writeToNBT(nbt);
+    }
+
+	
+	@Override
+	public boolean canConnect(ForgeDirection direction, Object source) 
+	{
+        if (direction == null || direction.equals(ForgeDirection.UNKNOWN))
+        {
+            return false;
+        }
+
+        return this.getInputDirections(direction);
+    }
+
+	private boolean getInputDirections(ForgeDirection direction) 
+	{
+		return direction != ForgeDirection.UP;
+	}
+
+	@Override
+    public long onReceiveEnergy(ForgeDirection from, long receive, boolean doReceive)
+    {
+        if (from != ForgeDirection.UNKNOWN && this.getInputDirections(from))
+        {
+        	return this.energy.receiveEnergy(receive, doReceive);
+        }
+
+        return 0;
+    }
+
+	@Override
+	public long onExtractEnergy(ForgeDirection from, long extract, boolean doExtract) 
+	{
+		return 0;
+	}
+
+    
+    public long setEnergy(ForgeDirection from, long energy)
+    {
+        this.energy.setEnergy(energy);
+        return energy;
+    }
+
+}
