b7198bed07d7934405f8a3ae0eb40aff7f09e328
Clarky158 <benclark.btc@gmail.com>
2014-05-16 12:54:30 +0100

    Create AtomValue.java

---
+++ electro/advAtomics/elements/AtomValue.java
@@ -0,0 +1,14 @@
+package * ;
+
+public class  AtomValue implements ElementValue, CompoundValue
+{
+      public AtomValue()
+      {
+      
+      }
+      
+      public String getElementName()
+      {
+          return null;      
+      }
+}
